4354b6b51416174725d4d5ae1704921c9e3b3a0d
Vladlrnv <rodilsyaletom@mail.ru>
2025-01-25 17:48:40 +0800

    home_contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home
+from catalog.views import home, contacts
 
-app = CatalogConfig.name
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home')
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        return HttpResponse("Сообщение отправлено")
+    else:
+        return render(request, 'contacts.html')
+
+
+
+
