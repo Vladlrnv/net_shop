841fd5719de8df9ba9f8745b39118b838ebe373d
Vladlrnv <rodilsyaletom@mail.ru>
2025-01-29 02:10:33 +0800

    work_with_image

--- catalog/models.py
+++ catalog/models.py
@@ -16,11 +16,11 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название продукта')
     description = models.TextField(verbose_name='Описание продукта', null=True, blank=True)
-    image = models.ImageField(upload_to='catalog/image', verbose_name='Изображение продукта', null=True, blank=True)
+    image = models.ImageField(upload_to='product/image', verbose_name='Изображение продукта', null=True, blank=True)
     category = models.ForeignKey(Category, models.CASCADE, verbose_name='Категория продукта', null=True, blank=True, related_name='products')
     price = models.IntegerField(verbose_name='Цена продукта')
-    created_at = models.DateField(verbose_name='Дата создания', null=True, blank=True, auto_now_add=True)
-    upload_at = models.DateField(verbose_name='Дата последнего изменения', null=True, blank=True, auto_now=True)
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    upload_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True)
 
     class Meta:
         verbose_name = 'Продукт'
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
@@ -11,3 +15,9 @@ def contacts(request):
         return HttpResponse("Сообщение отправлено")
     else:
         return render(request, 'contacts.html')
+
+
+def product_info(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'product_info.html', context)
