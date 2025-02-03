4aa25a7781fe67945a4b8e82ec9d2612b046be1b
Vladlrnv <rodilsyaletom@mail.ru>
2025-02-03 22:41:44 +0800

    pictures_view_fixed

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class BlogEntry(models.Model):
     header = models.CharField(max_length=50, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Содержимое', null=True, blank=True)
-    preview = models.ImageField(verbose_name='Превью', null=True, blank=True)
+    preview = models.ImageField(upload_to='blogentry/image', verbose_name='Превью', null=True, blank=True)
     created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
     publication_attribute = models.BooleanField(verbose_name='Признак публикации', default=False)
     quantity_views = models.IntegerField(verbose_name='Количество просмотров', default=0, null=True, blank=True)
